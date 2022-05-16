77f8416e35a316910298dc135bb3f3b92805c944
berna39 <kalemajoseph39@gmail.com>
2022-05-16 21:06:34 +0200

    Patch db paths

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_PATH, (err) => {
+mongoose.connect(process.env.LOCAL_DB_PATH, (err) => {
     if(err) { console.error(`Error detected : ${err.message}`); }
     else{ console.log('Database connected') }
 });
